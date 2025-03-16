ee09eae00c891c8248c8cfed923908485b00eb04
Robert <rmetcalf9@googlemail.com>
2025-03-16 13:13:44 +0000

    Converting to my release process

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -3,17 +3,21 @@
 set -ex
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-DIST_VERSION=$(git describe --abbrev=0 --tags)
+
+###DIST_VERSION=$(git describe --abbrev=0 --tags)
+echo "RJM - getting version"
+DIST_VERSION=$(cat ./VERSION )
+echo "RJM version got ${DIST_VERSION}"
 
 DIR=$(dirname "$0")
 
 # If the branch comes out as HEAD then we're probably checked out to a tag, so if the thing is *not*
 # coming out as HEAD then we're on a branch. When we're on a branch, we want to resolve ourselves to
 # a few SHAs rather than a version.
-if [[ $BRANCH != HEAD && ! $BRANCH =~ heads/v.+ ]]
-then
-    DIST_VERSION=$("$DIR"/get-version-from-git.sh)
-fi
+###if [[ $BRANCH != HEAD && ! $BRANCH =~ heads/v.+ ]]
+###then
+###    DIST_VERSION=$("$DIR"/get-version-from-git.sh)
+###fi
 
 DIST_VERSION=$("$DIR"/normalize-version.sh "$DIST_VERSION")
 VERSION=$DIST_VERSION yarn build
